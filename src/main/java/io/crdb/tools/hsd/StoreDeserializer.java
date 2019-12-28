e46d6524c128200fcaf1736438560d0919bb46ca
Tim Veil <3260845+timveil@users.noreply.github.com>
2019-12-28 12:39:43 -0500

    sorting collection of hot ranges

--- src/main/java/io/crdb/tools/hsd/StoreDeserializer.java
+++ src/main/java/io/crdb/tools/hsd/StoreDeserializer.java
@@ -5,12 +5,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.google.common.collect.Lists;
+import com.google.common.primitives.Floats;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class StoreDeserializer extends JsonDeserializer<Store> {
 
@@ -28,6 +26,10 @@ public class StoreDeserializer extends JsonDeserializer<Store> {
             HotRange hotRange = rangeNode.traverse(p.getCodec()).readValueAs(HotRange.class);
             hotRanges.add(hotRange);
         }
+
+        hotRanges.sort((o1, o2) -> Floats.compare(o1.getQueriesPerSecond(), o2.getQueriesPerSecond()));
+
+        Collections.reverse(hotRanges);
 
         return new Store(jsonNode.get("storeId").intValue(), hotRanges);
     }
