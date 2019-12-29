0b257160509c2f5bb054e9984c11e496d6a727e1
Tim Veil <3260845+timveil@users.noreply.github.com>
2019-12-29 17:39:44 -0500

    repackaging

--- src/main/java/io/crdb/shell/HotSpotShellProvider.java
+++ src/main/java/io/crdb/shell/HotSpotShellProvider.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class HotSpotShellProvider implements PromptProvider {
     @Override
     public AttributedString getPrompt() {
-        return new AttributedString("cockroach-utils:>",
+        return new AttributedString("crdb-shell:>",
                 AttributedStyle.DEFAULT.foreground(AttributedStyle.GREEN)
         );
     }
