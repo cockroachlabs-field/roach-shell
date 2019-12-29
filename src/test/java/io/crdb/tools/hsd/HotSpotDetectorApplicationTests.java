5152596775c1abdcec4da96291116eb80a28512d
Tim Veil <3260845+timveil@users.noreply.github.com>
2019-12-29 10:03:01 -0500

    cleanup

--- src/test/java/io/crdb/tools/hsd/HotSpotDetectorApplicationTests.java
+++ src/test/java/io/crdb/tools/hsd/HotSpotDetectorApplicationTests.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HotspotDetectorApplicationTests {
+class HotSpotDetectorApplicationTests {
 
     @Test
     void contextLoads() {
+        // no-op
     }
 
 }
