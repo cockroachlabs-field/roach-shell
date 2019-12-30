fcd6c440906e2a47294c423adb2be95d15a8b92d
Tim Veil <3260845+timveil@users.noreply.github.com>
2019-12-30 08:38:30 -0500

    clean

--- src/main/java/io/crdb/shell/HotSpotShellProvider.java
+++ src/main/java/io/crdb/shell/HotSpotShellProvider.java
@@ -1,7 +1,6 @@
 package io.crdb.shell;
 
 import org.jline.utils.AttributedString;
-import org.jline.utils.AttributedStyle;
 import org.springframework.shell.jline.PromptProvider;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +8,6 @@ import org.springframework.stereotype.Component;
 public class HotSpotShellProvider implements PromptProvider {
     @Override
     public AttributedString getPrompt() {
-        return new AttributedString("roach-shell:>",
-                AttributedStyle.DEFAULT.foreground(AttributedStyle.GREEN)
-        );
+        return new AttributedString("roach-shell:>");
     }
 }
