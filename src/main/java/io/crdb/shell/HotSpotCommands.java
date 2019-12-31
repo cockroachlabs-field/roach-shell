ab09a945a98070e1fb06e45f1d85d264011da0c9
Tim Veil <3260845+timveil@users.noreply.github.com>
2019-12-30 22:41:41 -0500

    cleanup

--- src/main/java/io/crdb/shell/HotSpotCommands.java
+++ src/main/java/io/crdb/shell/HotSpotCommands.java
@@ -15,27 +15,6 @@ public class HotSpotCommands {
         this.shellHelper = shellHelper;
     }
 
-    /*
-    final String host = environment.getRequiredProperty("crdb.host");
-        final int port = environment.getProperty("crdb.port", Integer.class, 26257);
-        final String database = environment.getProperty("crdb.database", "system");
-        final String username = environment.getProperty("crdb.username", "root");
-        final String password = environment.getProperty("crdb.password");
-        final String sslMode = environment.getProperty("crdb.ssl.mode", "disable");
-        final String crtPath = environment.getProperty("crdb.ssl.crt");
-        final String keyPath = environment.getProperty("crdb.ssl.key");
-        final String httpScheme = environment.getProperty("crdb.http.scheme");
-        final String httpUsername = environment.getProperty("crdb.http.username");
-        final String httpPassword = environment.getProperty("crdb.http.password");
-        final int httpPort = environment.getProperty("crdb.http.port", Integer.class, 8080);
-
-        final boolean secure = environment.getProperty("crdb.secure.enabled", Boolean.class, Boolean.FALSE);
-        final int maxHotRanges = environment.getProperty("crdb.hotranges.max", Integer.class, 10);
-
-
-        String httpHost = environment.getProperty("crdb.http.host");
-     */
-
     @ShellMethod("Find range Hot Spots")
     public void hotspots(@ShellOption(help = "hostname of crdb node") String host,
                          @ShellOption(help = "", defaultValue = "26257") int port,
@@ -52,6 +31,8 @@ public class HotSpotCommands {
                          @ShellOption(help = "", defaultValue = "8080") int httpPort,
                          @ShellOption(help = "", defaultValue = "false") boolean sslEnabled,
                          @ShellOption(help = "", defaultValue = "10") int maxRanges) {
+
+        // todo: print collected values
 
         HotSpotOptions hotSpotOptions = new HotSpotOptions();
         hotSpotOptions.setHost(host);
